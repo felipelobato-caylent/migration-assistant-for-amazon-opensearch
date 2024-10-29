29a8818e4d20a5d1912832a5d3a3fd0e01c08e69
Felipe Diego Lobato <felipe.silva@caylent.com>
2024-10-29 11:38:07 -0300

    Pointing initBootstrap.sh to the forked repository

--- source/solutions-infrastructure/initBootstrap.sh
+++ source/solutions-infrastructure/initBootstrap.sh
@@ -3,8 +3,8 @@
 yum update && yum install -y git java-11-amazon-corretto-devel docker nodejs https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_64bit/session-manager-plugin.rpm
 systemctl start docker
 git init
-release_tag=$(curl -s https://api.github.com/repos/opensearch-project/opensearch-migrations/releases/latest | jq -r ".tag_name")
-git remote add -f origin https://github.com/opensearch-project/opensearch-migrations.git
+release_tag=$(curl -s https://api.github.com/repos/felipelobato-caylent/opensearch-migrations/releases/latest | jq -r ".tag_name")
+git remote add -f origin https://github.com/felipelobato-caylent/opensearch-migrations.git
 git checkout tags/$release_tag
 
 cd deployment/cdk/opensearch-service-migration || exit
